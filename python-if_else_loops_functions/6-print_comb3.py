4501ff92d1c2de1a1991e78b7cef100b260bc61a
gitaumaggie <m.gitau1@alustudent.com>
2026-08-26 08:03:09 -0700

    modified task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for i in range(9):
         if i == 8 and j == 9:
             print("{:02d}".format(i * 10 + j))
         else:
-            print("{:02d}".format(i * 10 + j), end=",")
+            print("{:02d}".format(i * 10 + j), end=", ")
