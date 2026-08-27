79a032e273ebae86fd7cb1eab0e66a36583e5186
gitaumaggie <m.gitau1@alustudent.com>
2026-08-27 10:46:55 -0700

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize a Square with the given size."""
+        self.__size = size
