767b89f9910dc832bb0558ef05cfea616faa4981
gitaumaggie <m.gitau1@alustudent.com>
2026-09-16 16:18:04 +0400

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a function that returns an object's dictionary representation."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description of an object."""
+    return obj.__dict__
