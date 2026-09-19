6bf15a59aea9f77c969432af975c5bc0333960c7
gitaumaggie <m.gitau1@alustudent.com>
2026-09-20 01:10:02 +0400

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Count shows linked to each genre
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres
+ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id, genres.name
+ORDER BY number_of_shows DESC;
