8e7c9d8e05dc260ef6f41210a66a27afa6a94045
gitaumaggie <m.gitau1@alustudent.com>
2026-09-23 22:09:37 +0400

    Add task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with the required user ID header and displays the response body.
+# Sends a GET request to a URL with the header X-HolbertonSchool-User-Id set to 98 and displays the body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
