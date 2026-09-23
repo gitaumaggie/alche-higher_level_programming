64e87a6307b613592b2b5c1c32acb025ffd0e1b3
gitaumaggie <m.gitau1@alustudent.com>
2026-09-23 17:21:44 +0400

    modified task 1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the response body only for a 200 status code.
-curl -s -o /tmp/body -w '%{http_code}' "$1" | grep -q '^200$' && cat /tmp/body
+curl -s -w '%{http_code}' -o /tmp/body "$1" | grep -q 200 && cat /tmp/body
