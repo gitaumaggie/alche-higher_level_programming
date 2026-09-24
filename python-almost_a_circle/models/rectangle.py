4cedbabfde58313ec67dcb4aaa0600a5d9ffaae5
gitaumaggie <m.gitau1@alustudent.com>
2026-09-24 23:44:48 +0400

    Corrected the PEP8 formatting

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,6 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-
     def area(self):
         """Return the area of the rectangle."""
         return self.width * self.height
