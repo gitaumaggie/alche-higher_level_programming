4726832f3915f29754cb051928a8d0fe7bf19dd4
gitaumaggie <m.gitau1@alustudent.com>
2026-09-24 10:13:49 +0400

    Add task 10

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the GitHub user ID using Basic Authentication."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(
+        "https://api.github.com/user",
+        auth=(sys.argv[1], sys.argv[2])
+    )
+    print(response.json().get("id"))
