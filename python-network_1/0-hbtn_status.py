94188a937a148302b85dab70c8c406e92a7a48f4
gitaumaggie <m.gitau1@alustudent.com>
2026-09-24 09:40:01 +0400

    modified task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Fetches the status of the ALU Intranet and displays its response body."""
+"""Fetches the status of the intranet and displays its response body."""
 import urllib.request
 
 
-def fetch_status():
-    """Fetches the status page and displays its body information."""
-    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", body.decode("utf-8"))
-
-
-if __name__ == "__main__":
-    fetch_status()
