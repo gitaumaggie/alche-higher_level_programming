0c92f307e8538fbe5fd56123f0a50feed6171d9f
gitaumaggie <m.gitau1@alustudent.com>
2026-09-26 01:30:57 -0700

    Add task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List all cities and their corresponding states from a MySQL database."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+
+    query = (
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "INNER JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    connection.close()
