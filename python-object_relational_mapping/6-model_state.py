c2454a25ca7dfb23aa2aac4757933d96c6155dae
gitaumaggie <m.gitau1@alustudent.com>
2026-09-26 01:47:56 -0700

    Add task 6

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create database tables defined by SQLAlchemy models."""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
